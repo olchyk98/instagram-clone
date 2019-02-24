3ffb7c253d3eacdf3b34afdb020cfd05fb160de5
Oles Odynets <40524044+olchyk98@users.noreply.github.com>
2019-02-24 22:09:03 +0100

    MS. TT: Float photo modal > this.clientID by cookie === null

--- client/src/pages/__forall__/image.modal/index.js
+++ client/src/pages/__forall__/image.modal/index.js
@@ -4,6 +4,8 @@ import './main.css';
 import verification_image from '../images/verification.png';
 
 import client from '../../../apollo';
+import api from '../../../api';
+import { cookieControl } from '../../../utils';
 
 import { connect } from 'react-redux';
 import { gql } from 'apollo-boost';
@@ -12,9 +14,13 @@ import Comment from '../post.comment';
 import CommentInput from '../post.commentinput';
 
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faHeart, faComment, faBookmark } from '@fortawesome/free-regular-svg-icons';
+import {
+    faHeart as faHeartRegular,
+    faComment as faCommentRegular,
+    faBookmark as faBookmarkRegular
+} from '@fortawesome/free-regular-svg-icons';
 
-const image = "https://scontent-arn2-1.cdninstagram.com/vp/76899c4d53ad9917b9ca6d352b4df3f7/5D023554/t51.2885-15/e35/51246729_266849914212411_4636084601688378606_n.jpg?_nc_ht=scontent-arn2-1.cdninstagram.com";
+import loadingSpinner from '../loadingico.gif';
 
 class Hero extends Component {
     constructor(props) {
@@ -23,14 +29,22 @@ class Hero extends Component {
         this.state = {
             photo: null,
             active: false,
-            isLoading: false
+            isLoading: true
         }
+
+        this.viewRef = React.createRef();
+        this.clientID = null;
     }
 
     // TODO: Tag description
     // TODO: Multimedia scroll
     // TODO: Video play / pause
     // TODO: Erase data when user quits
+
+    componentDidMount() {
+        this.clientID = cookieControl.get("userid");
+        this.forceUpdate();
+    }
 
     componentDidUpdate(prevProps) {
         if(
@@ -42,16 +56,15 @@ class Hero extends Component {
                 this.props.activeData.id !== prevProps.activeData.id
             )
         ) { // UPDATED
-            console.log("UDPATED");
             if(this.props.activeData) {
-                this.fetchPhoto(this.props.activeData.id);
                 this.setState(() => ({
                     active: true,
                     isLoading: true
-                }));
+                }), () => this.fetchPhoto(this.props.activeData.id));
             } else {
                 this.setState(() => ({
-                    active: false
+                    active: false,
+                    isLoading: true
                 }));
             }
         }
@@ -62,7 +75,30 @@ class Hero extends Component {
             query: gql`
                 query($targetID: ID!) {
                     post(targetID: $targetID) {
-                        id
+                        id,
+                        media {
+                            id,
+                            url
+                        },
+                        creator {
+                            id,
+                            login,
+                            avatar,
+                            isVerified,
+                            isFollowing
+                        },
+                        comments {
+                            id,
+                            content,
+                            isLiked,
+                            creator {
+                                id,
+                                name
+                            }
+                        },
+                        likesInt,
+                        isLiked,
+                        time
                     }
                 }
             `,
@@ -73,7 +109,8 @@ class Hero extends Component {
             if(!post) return this.props.castError("We couldn't load this post. Please, try later.");
 
             this.setState(() => ({
-                photo: post
+                photo: post,
+                isLoading: false
             }));
         }).catch(console.error);
     }
@@ -85,87 +122,117 @@ class Hero extends Component {
                     className="gle-imagemodal-bg"
                     onClick={ this.props.closeSelf }
                 />
-                <div className="gle-imagemodal-mat">
-                    <section className="gle-imagemodal-mat-image">
-                        <img src={ image } alt="target" />
-                    </section>
-                    <section className="gle-imagemodal-mat-info">
-                        <div className="gle-imagemodal-mat-info-auth">
-                            <div className="gle-imagemodal-mat-info-auth-avatar">
-                                <img src={ image } alt="user" />
-                            </div>
-                            <span className="gle-imagemodal-mat-info-auth-name">oles.odynets</span>
-                            <div className="gle-imagemodal-mat-info-auth-verified">
-                                <img src={ verification_image } alt="This user was verified by finstagram administration" />
-                            </div>
-                            <span className="gle-imagemodal-mat-info-auth-split">•</span>
-                            <button className="gle-imagemodal-mat-info-auth-follow definp">Follow</button>
-                        </div>
-                        <div className="gle-imagemodal-mat-info-comments">
-                            <Comment />
-                            <button className="gle-imagemodal-mat-info-lcomments definp">
-                                Load more comments
-                            </button>   
-                            <Comment />
-                            <Comment />
-                            <Comment />
-                            <Comment />
-                            <Comment />
-                            <Comment />
-                            <Comment />
-                            <Comment />
-                            <Comment />
-                            <Comment />
-                            <Comment />
-                            <Comment />
-                            <Comment />
-                            <Comment />
-                            <Comment />
-                            <Comment />
-                            <Comment />
-                        </div>
-                        <div className="gle-imagemodal-mat-info-feedback">
-                            <div>
-                                <button className="definp gle-port-carousel-feedback-btn">
-                                    <FontAwesomeIcon icon={ faHeart } />
-                                </button>
-                                <button className="definp gle-port-carousel-feedback-btn">
-                                    <FontAwesomeIcon icon={ faComment } />
-                                </button>
-                            </div>
-                            <div>
-                                <button className="definp gle-port-carousel-feedback-btn">
-                                    <FontAwesomeIcon icon={ faBookmark } />
-                                </button>
-                            </div>
-                        </div>
-                        <button className="definp gle-imagemodal-mat-info-stricts">310 likes</button>
-                        <span className="gle-imagemodal-mat-info-postdate">1 day ago</span>
-                        <CommentInput
-                            className="gle-imagemodal-mat-info-commentinput"
-                            callMenu={() => this.props.callGlobalMenu({
-                                type: "OPTIONS",
-                                buttons: [
+                {
+                    (!this.state.isLoading) ? (
+                        <div className="gle-imagemodal-mat">
+                            <section className="gle-imagemodal-mat-image" ref={ ref => this.viewRef = ref }>
+                                <img src={ api.storage + this.state.photo.media[0].url } alt="target" />
+                            </section>
+                            <section className="gle-imagemodal-mat-info" ref={(ref) => {
+                                if(this.state.isLoading || !ref || !this.viewRef) return;
+                                ref.style.height = this.viewRef.getBoundingClientRect().height + "px";
+                            }}>
+                                <div className="gle-imagemodal-mat-info-auth">
+                                    <div className="gle-imagemodal-mat-info-auth-avatar">
+                                        <img src={ api.storage + this.state.photo.creator.avatar } alt="user" />
+                                    </div>
+                                    <span className="gle-imagemodal-mat-info-auth-name">{ this.state.photo.creator.login }</span>
                                     {
-                                        isRed: true,
-                                        action: () => null,
-                                        text: "Report the post"
-                                    },
-                                    {
-                                        isRed: false,
-                                        action: () => null,
-                                        text: "Copy Link"
-                                    },
-                                    {
-                                        isRed: false,
-                                        action: () => this.props.callGlobalMenu(null),
-                                        text: "Cancel"
+                                        (!this.state.photo.creator.isVerified) ? null : (
+                                            <div className="gle-imagemodal-mat-info-auth-verified">
+                                                <img src={ verification_image } alt="This user was verified by finstagram administration" />
+                                            </div>
+                                        )
                                     }
-                                ]
-                            })}
+                                    <span className="gle-imagemodal-mat-info-auth-split">•</span>
+                                    <button className="gle-imagemodal-mat-info-auth-follow definp">
+                                        {
+                                            (this.state.photo.creator.id !== this.clientID) ? (
+                                                (!this.state.photo.isFollowing) ? "Follow" : "Following"
+                                            ) : null
+                                        }
+                                    </button>
+                                </div>
+                                <div className="gle-imagemodal-mat-info-comments">
+                                    <Comment />
+                                    {/* content: PropTypes.string.isRequired, */}
+                                    {/* name: PropTypes.string.isRequired, */}
+                                    {/* canLike: PropTypes.bool, */}
+                                    {/* id: PropTypes.string.isRequired, */}
+                                    {/* isLiked: PropTypes.bool, */}
+                                    {/* checkTags: PropTypes.bool */}
+                                    <button className="gle-imagemodal-mat-info-lcomments definp">
+                                        Load more comments
+                                    </button>   
+                                    <Comment />
+                                    <Comment />
+                                    <Comment />
+                                    <Comment />
+                                    <Comment />
+                                    <Comment />
+                                    <Comment />
+                                    <Comment />
+                                    <Comment />
+                                    <Comment />
+                                    <Comment />
+                                    <Comment />
+                                    <Comment />
+                                    <Comment />
+                                    <Comment />
+                                    <Comment />
+                                    <Comment />
+                                </div>
+                                <div className="gle-imagemodal-mat-info-feedback">
+                                    <div>
+                                        <button className="definp gle-port-carousel-feedback-btn">
+                                            <FontAwesomeIcon icon={ faHeartRegular } />
+                                        </button>
+                                        <button className="definp gle-port-carousel-feedback-btn">
+                                            <FontAwesomeIcon icon={ faCommentRegular } />
+                                        </button>
+                                    </div>
+                                    <div>
+                                        <button className="definp gle-port-carousel-feedback-btn">
+                                            <FontAwesomeIcon icon={ faBookmarkRegular } />
+                                        </button>
+                                    </div>
+                                </div>
+                                <button className="definp gle-imagemodal-mat-info-stricts">{ this.state.photo.likesInt } likes</button>
+                                <span className="gle-imagemodal-mat-info-postdate">1 day ago</span>
+                                <CommentInput
+                                    className="gle-imagemodal-mat-info-commentinput"
+                                    callMenu={() => this.props.callGlobalMenu({
+                                        type: "OPTIONS",
+                                        buttons: [
+                                            {
+                                                isRed: true,
+                                                action: () => null,
+                                                text: "Report the post"
+                                            },
+                                            {
+                                                isRed: false,
+                                                action: () => null,
+                                                text: "Copy Link"
+                                            },
+                                            {
+                                                isRed: false,
+                                                action: () => this.props.callGlobalMenu(null),
+                                                text: "Cancel"
+                                            }
+                                        ]
+                                    })}
+                                />
+                            </section>
+                        </div>
+                    ) : (
+                        <img
+                            alt="loading spinner"
+                            style={{ zIndex: "5" }}
+                            className="glei-lspinner"
+                            src={ loadingSpinner }
                         />
-                    </section>
-                </div>
+                    )
+                }
             </div>
         );
     }
